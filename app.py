850183eaebe8cdda299e1b5f6b1d029793d3f06b
Lucas Jaccard <Lucas.jaccard1@gmail.com>
2025-10-14 16:21:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ st.markdown(
     }
     /* Make logo 4x bigger (default ~24px, so 96px height) */
     header img {
-        height: 65px !important;
+        height: 62px !important;
         width: auto !important;
     }
     </style>
